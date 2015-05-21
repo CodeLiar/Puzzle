6bd500526b4b230a4ff0de35bf8e3e971714bccd
丁明杰 <dingmingjie@babytree-inc.com>
2015-05-21 12:16:18 +0800

    add file src

--- PuzzleDemo/PuzzleDemo/RootViewController.h
+++ PuzzleDemo/PuzzleDemo/RootViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "LKAssetsLibrary.h"
 
 @interface RootViewController : UIViewController
 
--- PuzzleDemo/PuzzleDemo/RootViewController.m
+++ PuzzleDemo/PuzzleDemo/RootViewController.m
@@ -7,31 +7,88 @@
 //
 
 #import "RootViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface RootViewController ()
+
+@property (nonatomic, strong) LKAssetsLibrary *assetsLibrary;
+
+@property (nonatomic, strong) ALAssetsLibrary *sysLibrary;
 
 @end
 
 @implementation RootViewController
-
+#if 0
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(_assetsLibraryDidSetup:)
+                                                 name:LKAssetsLibraryDidSetupNotification
+                                               object:nil];
+    self.assetsLibrary = [LKAssetsLibrary assetsLibrary];
+    [self.assetsLibrary reload];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)_assetsLibraryDidSetup:(NSNotification *)notification
+{
+    NSLog(@"self.assetsLibrary %@", self.assetsLibrary.assetsGroups);
+    
+    LKAssetsGroup *assetsGroup = self.assetsLibrary.assetsGroups[0];
+    NSLog(@"assetsGroup %@", assetsGroup);
+    NSLog(@"%ld", (long)assetsGroup.assets.count);
+    
+    for (LKAsset *asset in assetsGroup.assets) {
+        NSLog(@"----> %@", asset);
+    }
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    _sysLibrary = [[ALAssetsLibrary alloc] init];
+    
+    dispatch_queue_t dispatchQueue =dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(dispatchQueue, ^(void) {
+        // 遍历所有相册
+        [self.sysLibrary enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+                                              // 遍历每个相册中的项ALAsset
+              [group enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index,BOOL *stop) {
+                  
+                  // ALAsset的类型
+                  NSString *assetType = [result valueForProperty:ALAssetPropertyType];
+                  if ([assetType isEqualToString:ALAssetTypePhoto]){
+                      ALAssetRepresentation *assetRepresentation =[result defaultRepresentation];
+                      CGFloat imageScale = [assetRepresentation scale];
+                      UIImageOrientation imageOrientation = (UIImageOrientation)[assetRepresentation orientation];
+                      dispatch_async(dispatch_get_main_queue(), ^(void) {
+                          CGImageRef imageReference = [assetRepresentation fullResolutionImage];
+                          // 对找到的图片进行操作
+                          UIImage *image =[[UIImage alloc] initWithCGImage:imageReference scale:imageScale orientation:imageOrientation];
+                          if (image != nil){
+                              NSLog(@"%@", image);
+                          } else {
+                              NSLog(@"Failed to create the image.");
+                          } });
+                  }
+              }];
+          }
+        failureBlock:^(NSError *error) {
+            NSLog(@"Failed to enumerate the asset groups.");
+        }];
+        
+    });
 }
-*/
+#endif
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+}
 
 @end
